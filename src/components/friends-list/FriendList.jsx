b707bc03d03ba384f6efe99825a52af8c18244a6
Iryna <irinkasatanovskaya@gmail.com>
2023-05-24 11:59:40 +0300

    styles updated

--- src/components/friends-list/FriendList.jsx
+++ src/components/friends-list/FriendList.jsx
@@ -6,7 +6,9 @@ export const FriendList = ({ friends }) => {
       {friends.map(friend => {
         return (
           <li key={friend.id} className={css.item}>
-            <span className="status">{friend.isOnline}</span>
+            <span className={friend.isonline ? css.green : css.red}>
+              {friend.isOnline}
+            </span>
             <img
               className="avatar"
               src={friend.avatar}
